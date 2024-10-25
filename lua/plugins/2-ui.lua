1503a5d2d6cf5b0691601593af13e9c1710f618d
keremimo <nyaa@live.com>
2024-10-25 21:16:31 +0200

    Update config

--- lua/plugins/2-ui.lua
+++ lua/plugins/2-ui.lua
@@ -39,6 +39,15 @@ return {
       },
     }
   },
+  { "catppuccin/nvim",
+    name = "catppuccin",
+    event = "User LoadColorSchemes",
+    flavour = "mocha",
+    priority = 1000,
+    opts = {
+      transparent_background = true
+    }
+  },
 
   --  astrotheme [theme]
   --  https://github.com/AstroNvim/astrotheme
@@ -625,51 +634,51 @@ return {
       },
     },
   },
-
-  --  mini.animate [animations]
-  --  https://github.com/echasnovski/mini.animate
-  --  HINT: if one of your personal keymappings fail due to mini.animate, try to
-  --        disable it during the keybinding using vim.g.minianimate_disable = true
-  {
-    "echasnovski/mini.animate",
-    event = "User BaseFile",
-    enabled = not is_android,
-    opts = function()
-      -- don't use animate when scrolling with the mouse
-      local mouse_scrolled = false
-      for _, scroll in ipairs { "Up", "Down" } do
-        local key = "<ScrollWheel" .. scroll .. ">"
-        vim.keymap.set({ "", "i" }, key, function()
-          mouse_scrolled = true
-          return key
-        end, { expr = true })
-      end
-
-      local animate = require("mini.animate")
-      return {
-        open = { enable = false }, -- true causes issues on nvim-spectre
-        resize = {
-          timing = animate.gen_timing.linear { duration = 33, unit = "total" },
-        },
-        scroll = {
-          timing = animate.gen_timing.linear { duration = 50, unit = "total" },
-          subscroll = animate.gen_subscroll.equal {
-            predicate = function(total_scroll)
-              if mouse_scrolled then
-                mouse_scrolled = false
-                return false
-              end
-              return total_scroll > 1
-            end,
-          },
-        },
-        cursor = {
-          enable = false, -- We don't want cursor ghosting
-          timing = animate.gen_timing.linear { duration = 26, unit = "total" },
-        },
-      }
-    end,
-  },
+  --
+  -- --  mini.animate [animations]
+  -- --  https://github.com/echasnovski/mini.animate
+  -- --  HINT: if one of your personal keymappings fail due to mini.animate, try to
+  -- --        disable it during the keybinding using vim.g.minianimate_disable = true
+  -- {
+  --   "echasnovski/mini.animate",
+  --   event = "User BaseFile",
+  --   enabled = not is_android,
+  --   opts = function()
+  --     -- don't use animate when scrolling with the mouse
+  --     local mouse_scrolled = false
+  --     for _, scroll in ipairs { "Up", "Down" } do
+  --       local key = "<ScrollWheel" .. scroll .. ">"
+  --       vim.keymap.set({ "", "i" }, key, function()
+  --         mouse_scrolled = true
+  --         return key
+  --       end, { expr = true })
+  --     end
+  --
+  --     local animate = require("mini.animate")
+  --     return {
+  --       open = { enable = false }, -- true causes issues on nvim-spectre
+  --       resize = {
+  --         timing = animate.gen_timing.linear { duration = 33, unit = "total" },
+  --       },
+  --       scroll = {
+  --         timing = animate.gen_timing.linear { duration = 50, unit = "total" },
+  --         subscroll = animate.gen_subscroll.equal {
+  --           predicate = function(total_scroll)
+  --             if mouse_scrolled then
+  --               mouse_scrolled = false
+  --               return false
+  --             end
+  --             return total_scroll > 1
+  --           end,
+  --         },
+  --       },
+  --       cursor = {
+  --         enable = false, -- We don't want cursor ghosting
+  --         timing = animate.gen_timing.linear { duration = 26, unit = "total" },
+  --       },
+  --     }
+  --   end,
+  -- },
 
   --  highlight-undo
   --  https://github.com/tzachar/highlight-undo.nvim
